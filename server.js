6ae8b76584584ccc3431fe2950301a4b286b278b
Anuraganu22 <anurag.k@techversantinfo.com>
2022-07-01 12:36:01 +0530

    changes

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const jsonServer = require("json-server")
 const server =jsonServer.create()
 const router =jsonServer.router('data/db.json')
 const middleware=jsonServer.defaults()
-
+const PORT =4000
 
 server.use(middleware)
 server.use(router)
 server.listen(4000,()=>{
-    console.log("sucessfully running")
+    console.log(`server running on port : ${PORT}`)
 })
 
